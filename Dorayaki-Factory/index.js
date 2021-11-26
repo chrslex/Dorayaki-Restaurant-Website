5e22715eac34fb3e3db3f46f73c8b5af93497f36
alwanpro <13519005@std.stei.itb.ac.id>
2021-11-26 11:01:26 +0700

    complete jws login logic

--- Dorayaki-Factory/index.js
+++ Dorayaki-Factory/index.js
@@ -29,13 +29,15 @@ app.get('/posts', authenticateToken, (req,res) =>{
     res.json(posts.filter(post => post.username === req.user.name))
 })
 
+let refreshTokens = []
+
 app.post('/login', (req,res)=>{
     //authentication
     const username = req.body.username
     const user = { name: username}
     const accessToken = generateAccessToken(user)
     const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
-    refreshToken.push
+    refreshTokens.push(refreshToken)
     res.json({accessToken: accessToken, refreshToken: refreshToken})
 
 })
@@ -45,7 +47,7 @@ app.post('/token', (req,res)=>{
     if (refreshToken == null) {
         return res.sendStatus(401)
     }
-    if (!refreshToken.includes(refreshToken)) {
+    if (!refreshTokens.includes(refreshToken)) {
         return res.sendStatus(403)
     }
     jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err,user) => {
@@ -55,13 +57,18 @@ app.post('/token', (req,res)=>{
     })
 })
 
+app.delete('/logout', (req, res) => {
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+    res.sendStatus(204)
+})
+
 app.listen(5000, ()=>{
     console.log("Server running successfuly");
 })
 
 function authenticateToken(req, res, next){
     const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split('')[1]
+    const token = authHeader.split(' ')[1]
     if (token == null) {return res.sendStatus(401)}
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,user) => {
         if (err) return res.sendStatus(403)
@@ -71,5 +78,5 @@ function authenticateToken(req, res, next){
 }
 
 function generateAccessToken(user) {
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1m'})
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30s'})
 }
